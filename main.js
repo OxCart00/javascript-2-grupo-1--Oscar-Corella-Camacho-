0ffe20b47ed623a2b64ce45674d4548d064320e4
OxCart00 <107891625+OxCart00@users.noreply.github.com>
2023-01-31 20:37:52 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+// Ejercicio 1
+
+// let number = 123;
+// function rev(numb) {
+//   let reversenumb = parseFloat(numb.toString().split('').reverse().join(''))* Math.sign(numb);
+//   return reversenumb;
+// }
+// console.log(number);
+// console.log(rev(number));
+
+// =================================================================================================================================
+
+// // Ejercicio 2
+
+// let word = "coloque aqui la oracion a modificar";
+// function up(item) {
+//   uppercaseword = item.split('')
+//   for (let i = 0; i < uppercaseword.length; i++) {
+//     if (uppercaseword[i] === " ") {
+//       uppercaseword[i+1] = uppercaseword[i+1].toUpperCase();
+//     }
+//   }
+//   return uppercaseword.join('');
+// }
+// console.log(word);
+// console.log(up(word));
+
