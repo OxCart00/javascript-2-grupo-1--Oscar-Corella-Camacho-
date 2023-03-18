f3078d6895d4ede82a551bed22de4521e5d9108c
OxCart00 <107891625+OxCart00@users.noreply.github.com>
2023-03-18 08:37:11 -0600

    Update

--- main.js
+++ main.js
@@ -1,99 +1,182 @@
-class libreria {
-  constructor(nombre, direccion) {
-    this.nombre = nombre;
-    this.direccion = direccion;
-    this.libros = [];
-    this.comics = [];
-  }
-  set agregarLibro(libroN) {
-    this.libros += " " + libroN + " ";
-  }
-  set agregarComic(comicN) {
-    this.comics += " " + comicN + " ";
-  }
-  display(){
-    console.log("Nombre: "+this.nombre,"Dirección: " + this.direccion,"Libros: " + this.libros,"Comics: " + this.comics);
-  }
-}
+import Bookstore from './modulos/Bookstore.js';
+import Book from './modulos/Book.js';
+import Comic from './modulos/Comic.js';
 
-// Nueva instancia de la clase libreria
-let libreriaUno = new libreria("Linda Vista", "200 mts oeste del cruce L.V. - R.A.");
+(function () {
+  const bookstore1 = new Bookstore('Bookstore', 'LV');
+  const sidebar = document.getElementById('sidebar');
+  const menu = ['Show Books', 'Show Comics', 'Add Book', 'Add Comic'];
 
-// Nuevo libro a la lista de libros
-libreriaUno.agregarLibro = "Corazón Delator";
-libreriaUno.agregarLibro = "The fault in our star";
-libreriaUno.agregarLibro = "Sherlock Holmes";
-libreriaUno.agregarLibro = "El cuervo";
-
-// Nuevo comic a la lista de comics
-libreriaUno.agregarComic = "Nanatsu No Taisai";
-libreriaUno.agregarComic = "Shigatsu Wa Kimi No Uso";
-libreriaUno.agregarComic = "Kimetsu No Yaiba";
-
-// Imprimir informacion de la libreria
-libreriaUno.display();
-
-class libro {
-  #titulo = "";
-  #autor = "";
-  #precio = 0;
-  #cantidad = 0;
-  #año = 0;
-  constructor(titulo, autor, precio, cantidad, año) {
-    this.#titulo = titulo;
-    this.#autor = autor;
-    this.#precio = precio;
-    this.#cantidad = cantidad;
-    this.#año = año;
+  const initialize = () => {
+    sidebar.insertAdjacentHTML(
+      'afterbegin',
+      `<h2 class="title">
+      ${bookstore1.getName}
+      </h2>`
+    );
+    sidebar.insertAdjacentHTML(
+      'beforeend',
+      `<ul class="menu">${createMenu()}</ul>`
+    );
+    menuFunctionality();
   }
 
-  set setCantidad (valor){
-    if (valor < 0) {
-      valor = 0;
+  const createMenu = () => {
+    let menuHtml = '';
+    menu.forEach((elem, index) => {
+      menuHtml += `<li class="menu-item"> 
+        <button id="${index}">${elem}</button>
+      </li>`
+    });
+
+    return menuHtml;
+  }
+
+  const menuFunctionality = () => {
+    const items = document.querySelectorAll('.menu-item');
+    items.forEach((item) => {
+      item.addEventListener('click', (event) => {
+        event.preventDefault(); // esto es para evitar que se recargue la página cuando se hace click en el elemento, en este al elemeto li del menu
+
+        const { id } = event.target
+            const mainContent = document.getElementById('main-content');
+        switch (id) {
+          
+          case '0':
+            showBooks();
+            addBookForm.style.display = "none";
+            addComicForm.style.display = "none";
+            break;
+          case '1':
+            showComics();
+            addBookForm.style.display = "none";
+            addComicForm.style.display = "none";
+            break;
+          case '2':
+            addBookForm.style.display = "block";
+            addComicForm.style.display = "none";
+            mainContent.innerHTML = '';
+            break;
+          case '3':
+            addComicForm.style.display = "block";
+            addBookForm.style.display = "none";
+            mainContent.innerHTML = '';
+            break;
+          default:
+            break;
+        }
+      });
+    });
+  }
+
+  const addBooks = (title, author, price, stock, year) => {
+    if (title && author && !isNaN(price) && !isNaN(stock) && !isNaN(year)) {
+      const book = new Book(title, author, price, stock, year);
+
+      const repeatedBook = bookstore1.getBooks.find(b => b.title === title);
+      if (repeatedBook) {
+        alert('The book already exists.');
+        return;
+      }
+      bookstore1.setBooks = book.getInfo();
+    } else {
+      alert('enter valid values.');
     }
-    this.#cantidad = valor;
+    
   }
-
-  set setAño (valor){
-    this.#año = valor;
+  const showBooks = () => {
+    const books = bookstore1.getBooks;
+    const bookElements = viewBook(books);
+    const mainContent = document.getElementById('main-content');
+    mainContent.innerHTML = '';
+    mainContent.insertAdjacentHTML('beforeend', bookElements);
   }
+  const viewBook = (books) => {
+    let bookHtml = '';
+    books.forEach((book) => {
+      bookHtml += `
+      <div class="book">
+      <p>title: ${book.title}</p>
+      <p>author: ${book.author}</p>
+      <p>price:${book.price}</p>
+      <p>stock: ${book.stock}</p>
+      <p>year: ${book.year}</p>
+      </div>
+    `;
+    });
+    return bookHtml;
+  }
+  const addComics = (title, author, price, stock, year, illustrator, publisher, volume) => {
+    if (title && author && !isNaN(price) && !isNaN(stock) && !isNaN(year) && illustrator && publisher && !isNaN(volume)) {
+      const comic = new Comic(title, author, price, stock, year,illustrator,publisher,volume);
 
-  get getInfo (){
-    if (this.#cantidad == 0) {
-      this.#cantidad = "No hay ejemplares disponibles"
+      const repeatedComic = bookstore1.getComics.find(b => b.title === title);
+      if (repeatedComic) {
+        alert('The book already exists.');
+        return;
+      }
+      bookstore1.setComics = comic.getInfo();
+    } else {
+      alert('enter valid values.');
     }
-    return this.#titulo + " " + this.#autor + " " + this.#precio + " " + this.#cantidad + " " + this.#año  + " ";
+    
   }
-}
-
-let libroUno = new libro ("Corazon Delator", "Edgar Allan Poe", 15000, 5, 1990);
-
-libroUno.setCantidad = 30;
-
-libroUno.setAño = 1890;
-
-console.log(libroUno.getInfo);
-
-class comic extends libro {
-  #dibujante = "";
-  #editorial = "";
-  #volumen = 0;
-  constructor(titulo, autor, precio, cantidad, año, dibujante, editorial, volumen) {
-    super(titulo, autor, precio, cantidad, año);
-    this.#dibujante = dibujante;
-    this.#editorial = editorial;
-    this.#volumen = volumen;
+  const showComics = () => {
+    const comics = bookstore1.getComics;
+    const comicElements = viewComic(comics);
+    const mainContent = document.getElementById('main-content');
+    mainContent.innerHTML = '';
+    mainContent.insertAdjacentHTML('beforeend', comicElements);
   }
-  get extraInfo (){
-    return this.#dibujante + " " + this.#editorial + " " + this.#volumen;
+  const viewComic = (comics) => {
+    let comicHtml = '';
+    comics.forEach((comic) => {
+      comicHtml += `
+        <div class="comic">
+          <p>Title: ${comic.title}<p>
+          <p>Author: ${comic.author}</p>
+          <p>Price: ${comic.price}</p>
+          <p>Stock: ${comic.stock}</p>
+          <p>Year: ${comic.year}</p>
+          <p>Illustrator: ${comic.illustrator}</p>
+          <p>Publisher: ${comic.publisher}</p>
+          <p>Volume: ${comic.volume}</p>
+        </div>
+      `;
+    });
+    return comicHtml;
   }
-}
+  const addBookForm = document.getElementById('add-book-form');
+  addBookForm.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-let comicUno = new comic ("Shigatsu Wakimi No Uso", "Kiori Arima", 15000, 100, 2000, "Kiori Arima", "Santillana", 7);
+    const title = document.getElementById('bookTitle').value;
+    const author = document.getElementById('bookAuthor').value;
+    const price = document.getElementById('bookPrice').value;
+    const stock = document.getElementById('bookStock').value;
+    const year = document.getElementById('bookYear').value;
 
-comicUno.setCantidad = 220;
+    addBooks(title, author, price, stock, year);
+    addBookForm.reset();
+  });
+  const addComicForm = document.getElementById('add-comic-form');
+  addComicForm.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-comicUno.setAño = 2001;
+    const title = document.getElementById('comicTitle').value;
+    const author = document.getElementById('comicAuthor').value;
+    const price = document.getElementById('comicPrice').value;
+    const stock = document.getElementById('comicStock').value;
+    const year = document.getElementById('comicYear').value;
+    const illustrator = document.getElementById('illustrator').value;
+    const publisher = document.getElementById('publisher').value;
+    const volume = document.getElementById('volume').value;
 
-console.log(comicUno.getInfo + comicUno.extraInfo);
+    console.log(title,author,price,stock,year,illustrator,publisher,volume);
 
+    addComics(title, author, price, stock, year, illustrator, publisher, volume);
+    addComicForm.reset();
+  });
+
+  initialize();
+})();
